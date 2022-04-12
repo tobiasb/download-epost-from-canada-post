19721242548e66612d061fbd435e8fa5e0f3f5db
Tobias Boehm <tobias@avaaz.org>
2022-04-12 12:14:46 -0700

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# Canada Post ePost downloader
+
+Canada Post is shutting down their ePost offering. Unfortunately they don't have a way to bulk download all ePost already received. This script downloads all documents to your local machine one by one. To be able to make authenticated requests, you need to provide your ePost cookie.
+
+1. Open the developer tools of your favorite browser 
+2. Log in to https://www.canadapost-postescanada.ca/inbox/en#!/inbox
+3. Open the request to https://www.canadapost-postescanada.ca/inbox/rs/mailitem and get the value of the `Cookie` request header.
+
+Run `python download-epost.py <cookie value>` or `python download-epost.py <cookie value> <dest>`
---
+++ download-epost.py
@@ -0,0 +1,76 @@
+import re
+import sys
+from os import path
+
+import requests
+from requests.adapters import HTTPAdapter, Retry
+
+cookies = sys.argv[1]
+
+headers = {
+    "Cookie": cookies,
+}
+
+first_time = True
+page_size = 50
+offset = 0
+num_processed = 0
+num_total = 0
+
+s = requests.Session()
+retries = Retry(total=5, backoff_factor=1, status_forcelist=[502, 503, 504])
+s.mount("https://", HTTPAdapter(max_retries=retries))
+
+response = requests.get("https://www.canadapost-postescanada.ca/inbox/en", headers=headers)
+
+sso_tokens = re.findall('"sso-token" content="([a-z0-9\-]*)"', response.text)
+sso_token = [token for token in sso_tokens if token][0]
+print(f"CSRF token is {sso_token}")
+headers["csrf"] = sso_token
+
+while True:
+    url = f"https://www.canadapost-postescanada.ca/inbox/rs/mailitem?folderId=0&sortField=1&order=D&offset={offset}&limit={page_size}"
+    response = s.get(url, headers=headers)
+    if response.status_code != 200:
+        print(f"Request to {url} failed with HTTP {response.status_code} 🔥")
+        break
+
+    if "content-length" in response.headers and response.headers["content-length"] == "0":
+        print("Failed getting a response. Are you sure you put in your cookies and csrf token? 🤔")
+        exit()
+
+    data = response.json()
+
+    if first_time:
+        num_total = data["numTotal"]
+        first_time = False
+
+    if len(data["mailitemInfos"]) == 0:
+        break
+
+    for mail_item in data["mailitemInfos"]:
+        desc = mail_item["shortDescription"]
+        file_name = f'{desc}.{mail_item["mailItemID"]}.pdf'.replace("/", "-")
+        file_location = path.join(sys.argv[2] if len(sys.argv) > 2 else "", file_name)
+        if path.exists(file_location):
+            print(f"Already downloaded {file_location}, skipping 💪")
+            num_processed += 1
+            continue
+
+        response = s.get(
+            f'https://www.epost.ca/service/displayMailStream.a?importSummaryId={mail_item["mailItemID"]}',
+            headers=headers,
+        )
+        if response.status_code != 200:
+            print(f"Downloading {desc} failed with HTTP {response.status_code} 🔥")
+            continue
+        print(f"Downloaded {file_location} 👍")
+
+        with open(file_location, mode="bw") as f:
+            f.write(response.content)
+
+        num_processed += 1
+
+    offset += page_size
+
+print(f"Processed {num_processed} of {num_total}! 🎉")
